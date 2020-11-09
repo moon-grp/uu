bf2667381c4603bc44df55df1765d0d523a481d5
emohex <olumidemm@gmail.com>
2020-11-09 22:37:49 +0100

    added lottie files

---
+++ layouts/reg.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-main>
+    <v-container>
+      <nuxt />
+    </v-container>
+  </v-main>
+</template>
+
+<style scoped>
+body {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  overflow: hidden;
+  background: #ecf4ff;
+}
+</style>
---
+++ pages/signup.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="boxSpace">
+    <div class="text-uppercase fnt">join the wish-list.</div>
+    <v-form>
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="first"
+            label="First Name"
+            outlined
+          ></v-text-field>
+          <v-text-field
+            v-model="first"
+            label="First Name"
+            outlined
+          ></v-text-field>
+          <v-btn color="#6C63FF" class="fnt-p text-capitalize" dark>
+            join us
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+    <Lottie
+      :width="1024"
+      :height="1024"
+      :options="lottieOptions"
+      v-on:animCreated="handleAnimation"
+    />
+  </div>
+</template>
+
+<script>
+import Lottie from 'vue-lottie/src/lottie.vue'
+import * as animationData from '~/assets/ani2.json'
+export default {
+  layout: 'reg',
+   components: {
+        Lottie
+    },
+  data() {
+    return {
+      anim: null, // for saving the reference to the animation
+      lottieOptions: { animationData: animationData.default },
+    }
+  },
+  methods: {
+    handleAnimation: function (anim) {
+      this.anim = anim
+    },
+  },
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap');
+
+#boxSpace {
+  margin-top: 10%;
+  margin-bottom: 10%;
+}
+
+.fnt {
+  font-family: 'Poppins', sans-serif;
+  font-size: 40px;
+}
+
+.fnt-p {
+  font-family: 'Poppins', sans-serif;
+}
+</style>
