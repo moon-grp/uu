61d7e5c3c7703880f8dbb1edd81a4d926b48a3bb
emohex <olumidemm@gmail.com>
2020-11-20 23:46:32 +0100

    fixs to wish

--- pages/wish.vue
+++ pages/wish.vue
@@ -11,11 +11,15 @@
                 label="pen it down"
                 class="text-capitalize"
                 color="#6C63FF"
+                v-model="wish"
+                :error-messages="wishError"
+                @input="$v.wish.$touch()"
+                @blur="$v.wish.$touch()"
               ></v-textarea>
             </div>
             <v-card-actions class="mr-2 mb-2">
               <v-spacer></v-spacer>
-              <v-btn outlined class="text-capitalize" color="#6C63FF"
+              <v-btn outlined class="text-capitalize" @click="makeAWish" color="#6C63FF"
                 >make a wish</v-btn
               >
             </v-card-actions>
@@ -27,10 +31,54 @@
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, minLength, email, sameAs } from 'vuelidate/lib/validators'
 export default {
   layout: 'reg',
+  mixins: [validationMixin],
+
+  validations: {
+    wish: { required },
+  },
   data() {
-    return {}
+    return {
+      loading: false,
+      wish: null,
+    }
+  },
+  mounted() {
+    this.makeAWish()
+  },
+  methods: {
+    async makeAWish() {
+      //console.log(JSON.parse(localStorage.getItem('token')))
+
+      try {
+        this.loading=true
+        var token = JSON.parse(localStorage.getItem('token'))
+        this.$axios.setHeader('Authorization', token)
+        const res = await this.$axios.$post(
+          'https://thewishlist.herokuapp.com/api/v1/wishit',
+          {
+            wish:this.wish
+          }
+        )
+        console.log(res)
+        this.loading=false
+      } catch (error) {
+        console.log(error)
+        this.loading=false
+      }
+    },
+  },
+  computed: {
+    wishError() {
+      const errors = []
+      if (!this.$v.wish.$dirty) return errors
+      !this.$v.wish.required &&
+        errors.push('Aboii, you gatz wish something na...')
+      return errors
+    },
   },
 }
 </script>
